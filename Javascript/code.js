d969fa217245374cf9d00b8dadf134f87eb52276
Halcon11 <105290500+Halcon11@users.noreply.github.com>
2022-06-22 20:31:04 +0100

    Add files via upload

---
+++ Javascript/code.js
@@ -0,0 +1,6 @@
+const call = "Ayanlola Richard Oluwafemi;";
+const height = "6 Ft 5 Inches;";
+const country = "Nigeria";
+const combination =
+  "Name: " + call + " Height: " + height + " Country: " + country + ".";
+alert(combination);
